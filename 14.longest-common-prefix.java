0fa6feda9c5d172fa72503429c9736363e800f37
jatinkartik <jatintyagi43625@gmail.com>
2023-02-13 11:46:25 +0530

    question longest common prefix

---
+++ 14.longest-common-prefix.java
@@ -0,0 +1,23 @@
+import java.lang.reflect.Array;
+
+import javax.swing.AbstractAction;
+
+/*
+ * @lc app=leetcode id=14 lang=java
+ *
+ * [14] Longest Common Prefix
+ */
+
+// @lc code=start
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            if (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+        }
+        return prefix;
+    }
+}
+// @lc code=end
