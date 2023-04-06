7c38cdbedde1f45449ccc67e7ecc66935466dbd5
jatinkartik <jatintyagi43625@gmail.com>
2023-04-06 22:29:57 +0530

    little bit working

---
+++ 33.search-in-rotated-sorted-array.cpp
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=33 lang=cpp
+ *
+ * [33] Search in Rotated Sorted Array
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int start = 0;
+        int end = nums.size() - 1;
+        int mid = start + (end - start) / 2;
+        while (start < end)
+        {
+            mid = start + (end - start) / 2;
+            if (target == nums[mid])
+            {
+                return mid;
+            }
+            else if (target > nums[mid])
+            {
+                if (nums[mid] > nums[start])
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid;
+                }
+            }
+            else
+            {
+                if (nums[mid] < nums[end])
+                {
+                    end = mid;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
+// @lc code=end
